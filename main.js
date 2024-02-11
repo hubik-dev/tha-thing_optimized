df47532e3f3115e8d3e4c121466e5ebb11b9ff3b
Bence Hubik <hubik.bence@outlook.com>
2024-02-11 11:20:59 +0100

    init electron

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
